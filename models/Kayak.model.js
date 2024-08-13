b22c12f88dad0f13237cef2b15af5fd2f43489a3
Sven Ole Spindler <s.spindler@gmx.de>
2024-08-13 09:26:10 +0200

    some additional properties in Kayak model

--- models/Kayak.model.js
+++ models/Kayak.model.js
@@ -7,23 +7,41 @@ const kayakSchema = new Schema(
     ownerType: {
       type: String,
       required: true,
-      enum: ["club boat", "private boat"],
+      enum: ["Club Boat", "Private Boat"],
     },
     name: { type: String, required: true },
     model: { type: String, required: true },
-    type: { type: String, required: true, enum: ["Touring Kayak", "Sea Kayak", "Racing Kayak", "Wildwater Kayak"], },
-    material: { type: String, required: true, enum: ["Plastic", "Fiberglass", "Wood", "Kevlar/Carbon Fibre Laminate", "Other"] },
+    type: {
+      type: String,
+      required: true,
+      enum: ["Touring Kayak", "Sea Kayak", "Racing Kayak", "Wildwater Kayak"],
+    },
+    material: {
+      type: String,
+      required: true,
+      enum: [
+        "Plastic",
+        "Fiberglass",
+        "Wood",
+        "Kevlar/Carbon Fibre Laminate",
+        "Other",
+      ],
+    },
     characteristics: { type: String, required: true },
     seats: { type: Number, required: true },
-    paddlerSize: { type: String },
+    paddlerSize: { type: String, enum: ["small", "medium", "large"] },
     stability: { type: Number, required: true },
     speed: { type: Number, required: true },
+    capacity: { type: Number, required: true },
     hasBulkheads: { type: Boolean, required: true },
-    steering: { type: String, enum: ["Skeg", "Rudder", "Tiller", "None"] },
+    steering: {
+      type: String,
+      enum: ["Skeg", "Rudder", "Skudder", "Skeg & Rudder", "Tiller", "None"],
+    },
     description: { type: String, required: true },
     reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: "Review" }],
-    imageUrl: {type: String},
-    user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
+    imageUrl: { type: String },
+    user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
