092f833e2f16959f52722df45d93bd4034ae6ba6
Sven Ole Spindler <s.spindler@gmx.de>
2024-08-20 15:52:10 +0200

    require rental routes in app.js

--- app.js
+++ app.js
@@ -30,6 +30,9 @@ app.use("/", reviewRoutes);
 const userRoutes = require("./routes/user.routes");
 app.use("/", userRoutes);
 
+const userRoutes = require("./routes/rental.routes");
+app.use("/", rentalRoutes);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
