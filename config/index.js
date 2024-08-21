8f0217f8d7e14ca4d94bb733f04196d53b8281a2
Sven Ole Spindler <s.spindler@gmx.de>
2024-08-21 09:11:31 +0200

    add localhost to cors

--- config/index.js
+++ config/index.js
@@ -13,7 +13,7 @@ const cookieParser = require("cookie-parser");
 // unless the request is made from the same domain, by default express wont accept POST requests
 const cors = require("cors");
 
-const FRONTEND_URL = process.env.ORIGIN
+const FRONTEND_URL = process.env.ORIGIN | "http://localhost:5173";
 
 // console.log("FRONTEND_URL:", FRONTEND_URL); // Add this line to log the value of FRONTEND_URL
 
@@ -26,7 +26,7 @@ module.exports = (app) => {
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
-      origin: [FRONTEND_URL],
+      origin: [FRONTEND_URL] | "http://localhost:5173", // The URL of our frontend
       credentials: true // Allow credentials (cookies, authorization headers, etc.)
     })
   );
